392c4fcb5cad460b2af6d38148462b2d5bd555d1
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-11-01 09:38:08 +0000

    Update cb_contact.php

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -4,14 +4,14 @@
         <div class="row">
             <div class="col-md-6">
                 <ul class="fa-ul">
-                    <li><span class="fa-li"><i class="fa-solid fa-map-marker-alt"></i></span>
+                    <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-map-marker-alt"></i></span>
                         <?=get_field('head_office_address','options')?></li>
                     <li><span class="fa-li"><i class="fa-solid fa-phone"></i></span>
-                        <?=get_field('head_office_phone','options')?></li>
+                        <a href="tel:<?=get_field('head_office_phone','options')?>"><?=get_field('head_office_phone','options')?></a></li>
                     <li><span class="fa-li"><i class="fa-solid fa-fax"></i></span>
                         <?=get_field('head_office_fax','options')?></li>
                     <li><span class="fa-li"><i class="fa-solid fa-envelope"></i></span>
-                        <?=get_field('head_office_email','options')?></li>
+                        <a href="mailto:<?=get_field('head_office_email','options')?>"><?=get_field('head_office_email','options')?></a></li>
                 </ul>
             </div>
             <div class="col-md-6">
@@ -26,14 +26,14 @@
         <div class="row">
             <div class="col-md-6 order-1 order-md-2">
             <ul class="fa-ul">
-                    <li><span class="fa-li"><i class="fa-solid fa-map-marker-alt"></i></span>
+                    <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-map-marker-alt"></i></span>
                         <?=get_field('sales_office_address','options')?></li>
                     <li><span class="fa-li"><i class="fa-solid fa-phone"></i></span>
-                        <?=get_field('sales_office_phone','options')?></li>
+                        <a href="tel:<?=get_field('sales_office_phone','options')?>"><?=get_field('sales_office_phone','options')?></a></li>
                     <li><span class="fa-li"><i class="fa-solid fa-fax"></i></span>
                         <?=get_field('sales_office_fax','options')?></li>
                     <li><span class="fa-li"><i class="fa-solid fa-envelope"></i></span>
-                        <?=get_field('sales_office_email','options')?></li>
+                        <a href="mailto:<?=get_field('sales_office_email','options')?>"><?=get_field('sales_office_email','options')?></a></li>
                 </ul>
             </div>
             <div class="col-md-6 order-2 order-md-1">
