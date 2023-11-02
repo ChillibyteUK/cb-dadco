eeaff6a23332e620f8a6a41591d3cbc74c3779b2
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-11-02 12:29:46 +0000

    two col text background

--- page-templates/blocks/cb_two_col_text.php
+++ page-templates/blocks/cb_two_col_text.php
@@ -2,12 +2,14 @@
 $bg = get_field('background') == 'dark' ? 'bg--blue-300' : '';
 ?>
 <section class="two_col_text py-5 <?=$bg?>">
-    <div class="row g-4">
-        <div class="col-md-6">
-            <?=apply_filters('the_content',get_field('content_left'))?>
-        </div>
-        <div class="col-md-6">
-            <?=apply_filters('the_content',get_field('content_right'))?>
+    <div class="container-xl">
+        <div class="row g-4">
+            <div class="col-md-6">
+                <?=apply_filters('the_content',get_field('content_left'))?>
+            </div>
+            <div class="col-md-6">
+                <?=apply_filters('the_content',get_field('content_right'))?>
+            </div>
         </div>
     </div>
 </section>
