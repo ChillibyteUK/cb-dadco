260c9116c0acbd878bb9d789490f090cef434ca2
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-12-07 10:43:10 +0000

    Update cb-blocks.php

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -88,6 +88,9 @@ function core_image_block_type_args($args, $name)
     if ($name == 'core/heading') {
         $args['render_callback'] = 'modify_core_add_container';
     }
+    if ($name == 'core/list') {
+        $args['render_callback'] = 'modify_core_add_container';
+    }
 
     return $args;
 }
