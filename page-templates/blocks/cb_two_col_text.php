7ca4726e8729e10609251d9f7e81e090c0a424bc
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-01-22 17:22:19 +0000

    two col text images

--- page-templates/blocks/cb_two_col_text.php
+++ page-templates/blocks/cb_two_col_text.php
@@ -5,10 +5,26 @@ $bg = get_field('background') == 'dark' ? 'bg--blue-300' : '';
     <div class="container-xl">
         <div class="row g-4">
             <div class="col-md-6" data-aos="fade-right">
-                <?=apply_filters('the_content',get_field('content_left'))?>
+                <?php
+                if (get_field('image_left')) {
+                    ?>
+                <img src="<?=wp_get_attachment_image_url(get_field('image_left'), 'large')?>"
+                    alt="" class="two_col_text__image">
+                <?php
+                }
+?>
+                <?=apply_filters('the_content', get_field('content_left'))?>
             </div>
             <div class="col-md-6" data-aos="fade-left">
-                <?=apply_filters('the_content',get_field('content_right'))?>
+                <?php
+if (get_field('image_right')) {
+    ?>
+                <img src="<?=wp_get_attachment_image_url(get_field('image_right'), 'large')?>"
+                    alt="" class="two_col_text__image">
+                <?php
+}
+?>
+                <?=apply_filters('the_content', get_field('content_right'))?>
             </div>
         </div>
     </div>
