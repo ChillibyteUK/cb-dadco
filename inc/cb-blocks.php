a4866f92325aef8c77166d45547ae994987fff3a
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-02-13 16:42:01 +0000

    pdf block

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -101,6 +101,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'cb_pdf',
+            'title'				=> __('CB PDF'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/cb_pdf.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
